49439f398a90091718eb1087d347113cfe313e67
Ross Jameson <Rossjameson1623@gmail.com>
2019-07-09 07:54:51 -0500

    working on ajax

--- assets/giphy.js
+++ assets/giphy.js
@@ -26,6 +26,25 @@ $(document).ready(function(){
     $(document.onabort("click", ".hero-button", function(){
         $("#images").empty();
 
-        $(".hero-button").remove
-    }))
+        $(".hero-button").removeClass("active");
+        let type = $(this).attr("data-type");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+            url:queryURL,
+            methed: "GET"
+        })
+        .then(function(response){
+            let results = response.data;
+            for (var i = 0; i < results.lenth; i++){
+                let heroDiv = $("<div class=\"hero-item\">")
+
+                let rating = results[i].rating;
+
+                let p = $("<p>").text("Rating: " + rating);
+                let animated = results[i].images.fixed_height.url;
+                let still = results[i].images.fixed_height.url;
+            }
+        })
+        }))
 })
